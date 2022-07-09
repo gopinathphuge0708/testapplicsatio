4ff8f9327a449fb787b5ce7a1ee416ccada5c5e8
Gopinath <phuge0708@gmail.com>
2022-07-09 13:17:38 +0530

    initial commit

---
+++ Test.java
@@ -0,0 +1,40 @@
+package com.csi.testes;
+
+import javax.swing.DefaultBoundedRangeModel;
+
+import org.apache.catalina.valves.rewrite.Substitution.StaticElement;
+import org.apache.jasper.runtime.ProtectedFunctionMapper;
+
+public class Test{
+	
+	static final int a = 10;
+	
+		public static void main(String[] args) {
+			
+		a = 20;
+			
+			System.out.println(a);
+			
+		}
+	}
+			
+			
+			
+
+		
+		
+			
+			
+			
+			
+		
+		
+		
+	
+			
+		
+		
+	
+	
+
+
